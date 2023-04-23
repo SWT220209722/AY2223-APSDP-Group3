e91cc9f6461bc72323f083e039705ab411eac952
JasonWong0315 <127303872+JasonWong0315@users.noreply.github.com>
2023-04-24 00:15:14 +0800

    add ui+arcgerGame

---
+++ Pool Area/Assets/AG/Sctipt/ArrowShoot.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class ArrowShoot: MonoBehaviour
+{
+    public GameObject ArrowPrefab;
+    RaycastHit hit; 
+    float range = 1000f;
+    public Transform ArrowSpawnPosition;
+    
+    //public GameObject HandArrow;
+
+    void shoot()
+    {
+        
+        //HandArrow.gameObject.SetActive(false);
+    
+        Vector2 ScreenCenter = new Vector2(Screen.width / 2f,Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(ScreenCenter);
+        if(Physics.Raycast(ray, out hit, range))
+        {
+            GameObject ArrowInstantiate = GameObject.Instantiate(ArrowPrefab, ArrowSpawnPosition.transform.position, ArrowSpawnPosition.transform.rotation) as GameObject;
+            ArrowInstantiate.GetComponent<Arrow>().setTarget(hit.point);
+        }
+    }
+}
