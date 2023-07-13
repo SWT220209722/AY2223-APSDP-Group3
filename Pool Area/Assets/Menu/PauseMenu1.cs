f3317043b92807fc6db2a5b5e8363c831941d56f
JasonWong0315 <127303872+JasonWong0315@users.noreply.github.com>
2023-07-14 02:39:28 +0800

    add archery Tuto

--- Pool Area/Assets/Menu/PauseMenu1.cs
+++ Pool Area/Assets/Menu/PauseMenu1.cs
@@ -4,12 +4,30 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu1 : MonoBehaviour {
 
+public GameObject pauseMenu;
+
+void Update()
+{
+    if (Input.GetKeyDown(KeyCode.Escape))
+    {
+       PauseGame (); // Pause the game by setting the time scale to 0
+		
+    }
+
+    if (Input.GetKeyDown(KeyCode.Space))
+    {
+        ResumeGame (); // Resume the game by setting the time scale back to 1
+    }
+}
+
 	public void PauseGame () {
 		Time.timeScale = 0;
+		//PauseMenu1.SetActive(true);
 	}
 
 	public void ResumeGame () {
 		Time.timeScale = 1;
+		//PauseMenu1.SetActive(false);
 	}
 
 	public void ExitGame () {
